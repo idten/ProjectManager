baa37fdc121e7bf246d38e37b4bb5bc508108200
idten <luel@naver.com>
2022-09-17 10:55:04 +0900

    commit

---
+++ src/test/java/com/ibk/epmo/EpmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibk.epmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpmoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
